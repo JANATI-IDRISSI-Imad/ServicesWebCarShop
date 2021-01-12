b4f582279b06091d0dde75108f9a367ab7be25bc
driss21 <drissnfifi41@gmail.com>
2021-01-13 00:26:00 +0100

    models/user(update)

--- src/main/java/fr/carchop/api/models/User.java
+++ src/main/java/fr/carchop/api/models/User.java
@@ -11,4 +11,9 @@ public class User {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
 
+    private String name;
+
+    private String email;
+    private String password;
+
 }
