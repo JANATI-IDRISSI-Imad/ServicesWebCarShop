0327127734c970996d1587034cc4d8b0ee032d6e
JANATI-IDRISSI-Imad <imad.janati.idrissi@gmail.com>
2021-01-12 17:53:40 +0100

    first commit

---
+++ src/main/java/fr/carchop/api/ServicesWebCarShopApplication.java
@@ -0,0 +1,13 @@
+package fr.carchop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesWebCarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesWebCarShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/carchop/api/ServicesWebCarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.carchop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesWebCarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
