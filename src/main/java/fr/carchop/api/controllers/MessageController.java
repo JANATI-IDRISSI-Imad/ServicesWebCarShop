a1ad98630376f22b3247d7353f9e71dc963ed899
Ghitamakhlas <Ghitamakhlass@gmail.com>
2021-03-20 13:32:57 +0100

    Fix contact us

--- src/main/java/fr/carchop/api/controllers/MessageController.java
+++ src/main/java/fr/carchop/api/controllers/MessageController.java
@@ -6,6 +6,7 @@ import fr.carchop.api.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class MessageController {
     @Autowired
     MessageService messageService;
 
-    @PostMapping("/CantactUs")
+    @PostMapping("/ContactUs")
     public void addCheckout(@RequestBody Message message){
         messageService.saveMessage(message);
     }
--- src/main/java/fr/carchop/api/models/Message.java
+++ src/main/java/fr/carchop/api/models/Message.java
@@ -18,11 +18,15 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private String fullName;
     private String email;
+    private String phone;
     private String message;
 
-    public Message(String email, String message) {
+    public Message(String fullName, String email, String phone, String message) {
+        this.fullName = fullName;
         this.email = email;
+        this.phone = phone;
         this.message = message;
     }
 }
