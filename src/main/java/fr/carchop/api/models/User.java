072c0e4355aa2ad209c361b8bcf4165e41c4d4c9
JANATI-IDRISSI-Imad <imad.janati.idrissi@gmail.com>
2021-03-22 17:25:22 +0100

    UserUpadte1

--- src/main/java/fr/carchop/api/models/User.java
+++ src/main/java/fr/carchop/api/models/User.java
@@ -25,5 +25,9 @@ public class User {
     @OneToMany(cascade = {CascadeType.ALL})
     public List<Notification> notification;
 
-
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
